7df001211d1fe2bfab601b7630ac70e969406617
Agustine <augustineg77@yahoo.com>
2026-03-17 20:43:57 +0530

    [FIX]: Deployment error on port is fixed

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 // Start scheduler
 startScheduler();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Payflo backend running on port ${PORT}`);
 });
